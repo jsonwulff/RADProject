0731fbf620ed18dfee92133852d3b1e34246fb4a
Mads Christensen <mads.bchrist@gmail.com>
2020-05-27 10:31:12 +0200

    fixed overflow i a og b variable

--- RADProject/ModPrime.cs
+++ RADProject/ModPrime.cs
@@ -12,12 +12,15 @@ namespace RADProject {
             p = BigInteger.Pow(2, 89) - 1;
             l = 10;
             Random rnd = new Random();
-            Byte[] a_bytes = new Byte[11];
-            Byte[] b_bytes = new Byte[11];
+            Byte[] a_bytes = new Byte[12];
+            Byte[] b_bytes = new Byte[12];
             rnd.NextBytes(a_bytes);
             rnd.NextBytes(b_bytes);
+            a_bytes[11] = 0x00;
+            b_bytes[11] = 0x00;
             a = new BigInteger(a_bytes);
             b = new BigInteger(b_bytes);
+            
         }
 
         public override ulong hash(ulong x) {
