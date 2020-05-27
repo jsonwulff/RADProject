49115d8410445ee31e02b1b905d292d0bf5ca6b1
ainulindaie <pelle.galloe@gmail.com>
2020-05-27 10:58:41 +0200

    Added constructor to ModPrime class - My computer generated some json files that maybe should be excluded

--- RADProject/ModPrime.cs
+++ RADProject/ModPrime.cs
@@ -8,9 +8,21 @@ namespace RADProject {
         public BigInteger p ;
         public int l;
 
-        public void hashgen() {
+        public ModPrime(int img, bool random){
             p = BigInteger.Pow(2, 89) - 1;
-            l = 10;
+            l = img;
+
+            if (random){
+                hashgen();
+            }else{
+                byte[] a_bytes = [0x32, 0x4c, 0x8e, 0xd8, 0x08, 0x8d, 0xb1, 0x13, 0xca, 0x4a, 0x22, 0x00];
+                byte[] b_bytes = [0x0d, 0xb5, 0x64, 0x33, 0xce, 0xf5, 0x7c, 0xdd, 0x63, 0x1a, 0xc8, 0x00];
+                a = new BigInteger(a_bytes);
+                b = new BigInteger(b_bytes);
+            }
+        }
+
+        public void hashgen() {
             Random rnd = new Random();
             Byte[] a_bytes = new Byte[12];
             Byte[] b_bytes = new Byte[12];
