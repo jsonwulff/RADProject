faef8849443f1bef6fd33fe43e929d04533ccc56
Julian Wulff <julianwulff@gmail.com>
2020-05-27 10:32:32 +0200

    Added code for testing running time

--- RADProject/Program.cs
+++ RADProject/Program.cs
@@ -9,6 +9,11 @@ namespace RADProject
             foreach (var tuple in Stream.CreateStream(100, 50)) {
                 Console.WriteLine(tuple);
             }
+            
+            // var watch = System.Diagnostics.Stopwatch.StartNew();
+            // // the code that you want to measure comes here
+            // watch.Stop();
+            // var elapsedMs = watch.ElapsedMilliseconds;
         }
     }
 }
