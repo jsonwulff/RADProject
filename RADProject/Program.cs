5f0ed40627aa6d2f4f712307726729fdc0cef319
Julian Wulff <julianwulff@gmail.com>
2020-06-02 12:46:45 +0200

    Added path to result folder

--- RADProject/Program.cs
+++ RADProject/Program.cs
@@ -2,33 +2,15 @@
 using RADProject.HashFunctions;
 using RADProject.HashTabel;
 using System.Diagnostics;
+using System.IO;
 using RADProject.CountSketch;
 
 namespace RADProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(">>>> OPGAVE 1 <<<<");
-
-            ModPrime multiplyModPrime = new ModPrime(63, false);
-            multiplyModPrime.TestMultiplyModPrime(1000000, 63);
-
-            MultiplyShift multiplyShift = new MultiplyShift(63, false);
-            multiplyShift.TestMultiplyShift(1000000, 63);
-
-
-            Hash mP = new ModPrime(8, false);
-            Hash mS = new MultiplyShift(8, false);
-            HashTable multShiftTable = new HashTable(256, mS);
-            HashTable modPrimeTable = new HashTable(256, mP);
-
-            foreach (var tuple in Stream.CreateStream(10000, 50, false)) {
-                multShiftTable.Increment(tuple.Item1, tuple.Item2);
-                modPrimeTable.Increment(tuple.Item1, tuple.Item2);
-            }
-
-            Console.WriteLine("multshift quadratic sum: " + multShiftTable.calcQuadSum());
-            Console.WriteLine("modPrime  quadratic sum: " + modPrimeTable.calcQuadSum());
-
+            Opgave1.Run();
+            Opgave3.Run();
+            
             int streamSize = 10000; //Should be updated to just below our limit.
             Console.WriteLine("Testing countsketch vs. true S value and outputting files");
             String s_str, mse_str, chi_str, m_str;
@@ -41,7 +23,7 @@ namespace RADProject {
                 ulong s = estimates.Item2;
                 ulong mse = 0UL;
                 ulong[] m = new ulong[9];
-
+            
                 //Calculates the M_i=mean(g_i) for i \in [9]
                 for (int i = 0; i < 9; i++){
                     ulong[] g_i = new ulong[11];
@@ -58,28 +40,26 @@ namespace RADProject {
                     }
                 }
                 mse = mse/100;
-
+            
                 //Sort the arrays
                 Array.Sort(chi_values);
                 Array.Sort(m);
-
+            
                 //Updates the strings for the save files
                 m_str += String.Join(",", m) + "\n";
                 mse_str += String.Format("{0}", mse) + "\n";
                 s_str += String.Format("{0}", s) + "\n";
                 chi_str += String.Join(",", chi_values) + "\n";
             }
+            
+            string projectDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string resultsDir = Path.Combine(projectDir, "Results");
 
-            //Not sure if it is a valid path
-            // string docPath = Environment.CurrentDirectory;
-            // string workingDirectory = Environment.CurrentDirectory;
-            // Console.WriteLine(workingDirectory);
-            // string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.FullName;
-            // Console.WriteLine(projectDirectory);
-            System.IO.File.WriteAllText(@"s_values.txt", s_str);
-            System.IO.File.WriteAllText(@"mse_values.txt", mse_str);
-            System.IO.File.WriteAllText(@"chi_values.txt", chi_str);
-            System.IO.File.WriteAllText(@"m_values.txt", m_str);
+            File.WriteAllText(Path.Combine(resultsDir,"s_values.txt"), s_str);
+            File.WriteAllText(Path.Combine(resultsDir,"mse_values.txt"), mse_str);
+            File.WriteAllText(Path.Combine(resultsDir,"chi_values.txt"), chi_str);
+            File.WriteAllText(Path.Combine(resultsDir,"m_values.txt"), m_str);
+
         }
     }
 }
