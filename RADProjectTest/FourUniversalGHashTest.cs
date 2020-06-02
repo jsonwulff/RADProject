388739040026c8f36b958532c977c24c031bd549
Julian Wulff <julianwulff@gmail.com>
2020-06-02 20:56:18 +0200

    Added random test with random A0,...,A3

--- RADProjectTest/FourUniversalGHashTest.cs
+++ RADProjectTest/FourUniversalGHashTest.cs
@@ -1,7 +1,6 @@
-using System;
 using NUnit.Framework;
 using RADProject;
-using  System.Numerics;
+using System.Numerics;
 
 namespace RADProjectTest {
     public class FourUniversalGHashTest {
@@ -10,19 +9,32 @@ namespace RADProjectTest {
 
         [Test]
         public void TestGHash() {
-            FourUniversal fourUniversal = new FourUniversal(63, false);
+            FourUniversal fourUniversal = new FourUniversal(13, false);
             BigInteger a0 = fourUniversal.A[0];
             BigInteger a1 = fourUniversal.A[1];
             BigInteger a2 = fourUniversal.A[2];
             BigInteger a3 = fourUniversal.A[3];
-            BigInteger p  = fourUniversal.P;
-            foreach (var tuple in Stream.CreateStream(100, 10, true)) {
-                ulong x = tuple.Item1;
-                BigInteger GHash = ((a0 % p )+ ((a1 * x) % p ) + ((a2 * (x * x)) % p) + (a3 * (x * x * x)) % p);
-                Assert.AreEqual(GHash, fourUniversal.g_hash(x));
+            BigInteger p = fourUniversal.P;
+            foreach (var tuple in Stream.CreateStream(100, 63, true)) {
+                BigInteger x = (BigInteger) tuple.Item1;
+                BigInteger GHash = (a0 + a1 * x % p + a2 * (x * x) + a3 * (x * x * x)) % p;
+                Assert.AreEqual(GHash, fourUniversal.GHash((ulong) x));
             }
-            
         }
-
+        
+        [Test]
+        public void TestRandomGHash() {
+            FourUniversal fourUniversal = new FourUniversal(13, true);
+            BigInteger a0 = fourUniversal.A[0];
+            BigInteger a1 = fourUniversal.A[1];
+            BigInteger a2 = fourUniversal.A[2];
+            BigInteger a3 = fourUniversal.A[3];
+            BigInteger p = fourUniversal.P;
+            foreach (var tuple in Stream.CreateStream(100, 63, true)) {
+                BigInteger x = (BigInteger) tuple.Item1;
+                BigInteger GHash = (a0 + a1 * x % p + a2 * (x * x) + a3 * (x * x * x)) % p;
+                Assert.AreEqual(GHash, fourUniversal.GHash((ulong) x));
+            }
+        }
     }
 }
