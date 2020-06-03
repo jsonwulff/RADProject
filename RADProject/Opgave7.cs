f823b3f031cdce3c8fa51925d0fc68fae7448cc1
ainulindaie <pelle.galloe@gmail.com>
2020-06-03 12:00:28 +0200

    Rewrote the entirety of Opgave7.cs - Full commenting still needed for understanding the inner workings

--- RADProject/Opgave7.cs
+++ RADProject/Opgave7.cs
@@ -1,37 +1,51 @@
 using System;
 using System.Numerics;
+using RADProject.Table;
 using RADProject.CountSketch;
+using RADProject.HashFunctions;
 
 namespace RADProject {
 
     public class Opgave7 {
-        
-        public static void Run(int streamSize, int l, int sL) {
+        private HashTable hashTable;
 
+        private CountSketch[] countSketchArray;
+
+        private ulong[] chiValues;
+
+        private ulong s;
+
+        private int streamInputSize;
+
+        private int streamImageSpace;
+
+        private int hashImageSpace;
+        private ulong m;
+        
+        public Opgave7(int streamSize, int l, int sL){
+            this.m = 1UL << l;
+            this.streamInputSize = streamSize;
+            this.streamImageSpace = sL;
+            this.hashImageSpace = l;
+        }
+        public void Run() {
             Console.WriteLine(">>>> OPGAVE 7 <<<<");
-            Console.WriteLine(String.Format(">> Stream size: {0}\t l: {1}", streamSize, l));
+            Console.WriteLine(String.Format(">> Stream size: {0}\t l: {1}", streamInputSize, hashImageSpace));
             
-            Console.WriteLine(">> Creating CSCalc with parameters.");
-            
-            CSCalc csc = new CSCalc(streamSize, sL, l);
-            Tuple<ulong[], ulong> estimates = csc.Estimates();
-            ulong[] chi_values = estimates.Item1;
-            ulong s = estimates.Item2;
-            
-            Console.WriteLine(String.Format("Output>> True S value: {0}", s));
+            this.CalculateEstimates();
 
             BigInteger mse = 0;
-            ulong[] m = new ulong[9];
+            ulong[] m_i_array = new ulong[9];
             
             Console.WriteLine(">> Calculating M_i for i \\in [9]:");
             //Calculates the M_i=mean(g_i) for i \in [9]
             for (int i = 0; i < 9; i++){
                 ulong[] g_i = new ulong[11];
-                Array.Copy(chi_values, (i*11), g_i, 0, 11);
+                Array.Copy(chiValues, (i*11), g_i, 0, 11);
                 Array.Sort(g_i);
-                m[i] = g_i[5];
+                m_i_array[i] = g_i[5];
 
-                String temp = String.Format("M_{0}: {1} \t G_{2}: ", i, m[i], i);
+                String temp = String.Format("M_{0}: {1} \t G_{2}: ", i, m_i_array[i], i);
                 Console.WriteLine(temp);
             }
 
@@ -41,10 +55,10 @@ namespace RADProject {
             for (int i = 0; i < 10; i++){
                 int index = i * 10;
                 for (int j = 0; j < 10; j++){
-                    BigInteger error = (int) (chi_values[index+j] - s);
+                    BigInteger error = (int) (chiValues[index+j] - s);
                     mse += (error * error);
 
-                    Console.WriteLine(String.Format("current sum of squared errors: {0} \t error from S={1} at Chi[{2}]={3} is: {4}", mse, s, (index+j), chi_values[index+j], error));
+                    Console.WriteLine(String.Format("current sum of squared errors: {0} \t error from S={1} at Chi[{2}]={3} is: {4}", mse, s, (index+j), chiValues[index+j], error));
                 }
             }
 
@@ -52,9 +66,46 @@ namespace RADProject {
             Console.WriteLine(String.Format("Final computed mean squared error: {0}", mse));
             
             //Sort the arrays
-            //Array.Sort(chi_values);
+            //Array.Sort(chiValues);
             //Array.Sort(m);
 
+        }
+
+        private void InstanciateCountSketchesAndHashTable(){
+            Console.WriteLine("Instanciating the Countsketches and the hash table");
+            MultiplyShift h = new MultiplyShift(hashImageSpace, true);
+            hashTable = new HashTable(m, h);
+            countSketchArray = new CountSketch[100];
+            chiValues = new ulong[100];
+
+            for (int i=0; i<100; i++){
+                FourUniversal g = new FourUniversal(hashImageSpace, true);
+                countSketchArray[i] = new CountSketch(m, g);
+            }
+        }
+
+        private void CalculateEstimates(){
+            InstanciateCountSketchesAndHashTable();
+
+            String msg = String.Format("Running over a stream of size n={0},\t with an imagespace of 2^{1} \n Incrementing the hashtable, and adding to all countsketches, (x, delta)", streamInputSize, streamImageSpace);
+            Console.WriteLine(msg);
+
+            foreach (var tuple in Stream.CreateStream(streamInputSize, streamImageSpace, true)){
+                hashTable.Increment(tuple.Item1, tuple.Item2);
+
+                foreach(CountSketch c in countSketchArray){
+                    c.Add(tuple.Item1, tuple.Item2);
+                }
+            }
+            
+            Console.WriteLine("Calculating S as quadSum");
+            s = hashTable.CalcQuadSum();
+            Console.WriteLine("S = " + s);
+
+            Console.WriteLine("Calculating Chi values");
+            for (int i = 0; i < 100; i++){
+                chiValues[i] = countSketchArray[i].Chi();
+            }
         }
     }
 }
